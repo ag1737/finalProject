66a26261d231e87ef6dde735b09a62449fb1a5c0
Adamg <ag1737@york.ac.uk>
2022-05-13 12:56:55 +0100

    init

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,17 +2,17 @@ import pandas as pd
 import numpy as np
 import json
 import pprint
+import os
 
 
 class DataLoader():
 
-    player_entities = []
-    hero_entities = []
-    map_entities = []
-
-
     def __int__(self, folder_location: str):
         file = open(folder)
 
+    def folder_parser(self, folder: str):
+        directory = os.fsencode(folder)
 
-    def
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            print(filename)
